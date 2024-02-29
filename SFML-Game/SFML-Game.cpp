43df184eda732dbb17b0d15b605dabcb5ee4ba47
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-02-29 16:01:31 +0100

    rectangle in the centre

--- SFML-Game/SFML-Game.cpp
+++ SFML-Game/SFML-Game.cpp
@@ -3,12 +3,15 @@
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
+using namespace sf;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
+    sf::RectangleShape rectangle (Vector2f(128.0f, 128.0f));
+    rectangle.setFillColor(sf::Color::Magenta);
+    rectangle.setPosition(320, 240);
+    rectangle.setOrigin(rectangle.getSize().x / 2, rectangle.getSize().y / 2); // centre the rectangle
 
     while (window.isOpen())
     {
@@ -20,7 +23,7 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(rectangle);
         window.display();
     }
 
