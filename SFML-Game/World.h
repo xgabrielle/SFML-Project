953c0267ddc1401402447a2525160520d6f7f991
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-03-04 15:06:21 +0100

    world. h, methods and variables

--- SFML-Game/World.h
+++ SFML-Game/World.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Snake.h"
+using namespace sf;
 class World
 {
+	Vector2u windowSize; // what does u stand for?
+	Vector2i appleItem;
+	int blockSize;
+
+	CircleShape appleSize;
+	RectangleShape bounds[4]; // not sure what this is?? is it the playerfield that is a rectangle so = 4 corners? or 4 walls?
+public:
+	World(Vector2u one_windSize); // wind??
+	~World();
+
+	int GetBlockSize();
+	void RespawnApple();
+	void Update(Snake& one_player);
+	void Render(RenderWindow& one_window);
 };
 
