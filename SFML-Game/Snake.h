e12cd010cd7c3ce385170effd8842ca4e7c5911c
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-03-04 07:43:39 +0100

    Implementing the snake structure

--- SFML-Game/Snake.h
+++ SFML-Game/Snake.h
@@ -1,5 +1,15 @@
 #pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+enum class Direction{None, Up, Down, Left, Right};
+using SnakeContainer = std::vector<SnakeSegment>;
+struct SnakeSegment
+{
+	SnakeSegment(int x, int y) : position(x, y) {};
+	sf :: Vector2i position;
+};
 class Snake
 {
+	
 };
 
