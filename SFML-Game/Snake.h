265142c03acb8f337df873df031acba284fefc06
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-03-04 09:04:22 +0100

    define methods n variables in snake.h

--- SFML-Game/Snake.h
+++ SFML-Game/Snake.h
@@ -3,13 +3,43 @@
 #include <SFML/Graphics.hpp>
 enum class Direction{None, Up, Down, Left, Right};
 using SnakeContainer = std::vector<SnakeSegment>;
+using namespace sf;
 struct SnakeSegment
 {
 	SnakeSegment(int x, int y) : position(x, y) {};
-	sf :: Vector2i position;
+	Vector2i position;
 };
 class Snake
 {
-	
+	void CheckCollision();
+	SnakeContainer snakeBody;
+	int graphicsSize;
+	Direction direction;
+	int snakeSpeed;
+	int lives;
+	int score;
+	bool hasLost;
+	RectangleShape bodyRectangle;
+
+public:
+	Snake(int one_blockSize);
+    ~Snake();
+
+	void SetDirection(Direction one_direction);
+	Direction GetDirection();
+	int GetSpeed();
+	Vector2i GetPosition();
+	int GetLives();
+	int GetScore();
+	void IncreaseScore();
+	bool HasLost();
+	void Lose();
+	void ToggleLost();
+	void Extend();
+	void Reset();
+	void Move();
+	void Tick();
+	void Cut(int one_segments);
+	void Render(RenderWindow one_window);
 };
 
