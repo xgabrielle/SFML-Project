d8a62b62dcf5916304f7e258ad04a747e6cb24a8
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-04-27 00:03:45 +0200

    moved around on the order

--- SFML-Game/Window.cpp
+++ SFML-Game/Window.cpp
@@ -3,10 +3,9 @@
 #include <SFML/Graphics.hpp>
 using namespace sf;
 using namespace std;
-// dick move not being able to have "window" as a name for a class???
+// not being able to have "window" as a name for a class???
 
 MyWindow::MyWindow() {Setup( "MyWindow", Vector2u(640, 480)); }
-
 MyWindow::MyWindow(const string& title, const Vector2u& size) // window size?
 {
 	Setup(title, size);
@@ -22,32 +21,46 @@ void MyWindow::Setup(const string& title, const Vector2u& size) // same Q here ?
 	Create();
 }
 
-RenderWindow& MyWindow::GetRenderWindow() { return window; }
 
 void MyWindow::Create() // this whole thing is a mess? .. read about it .. still don't get it ...
 {
-	/*auto style = (isFullscreen ? Style::Fullscreen
-		: Style::Default);
-	window.create({ windowSize.x, windowSize.y, 32 },
-		windowTitle, style);*/
-	auto style{0};
+	auto style{ 0 };
 	if (isFullscreen)
 	{
 		style = Style::Fullscreen; // does what it says .. i think??
 	}
-	
+
 	else
 	{
 		style = Style::Default; // can swap between  Titlebar | Resize | Close
-		
+
 	}
 	window.create({ windowSize.x,windowSize.y, 32 }, windowTitle, style); // what's happening here? invokes it??
 }
+
 void MyWindow::Destroy()
 {
 	window.close();
 }
-void MyWindow::WUpdate() 
+
+void MyWindow::BeginDraw() { window.clear(Color::Black); }
+void MyWindow::EndDraw() { window.display(); }
+
+bool MyWindow::IsDone() { return isDone; }
+bool MyWindow::IsFullscreen() { return isFullscreen; }
+
+RenderWindow& MyWindow::GetRenderWindow() { return window; }
+
+Vector2u MyWindow::GetWindowSize() { return windowSize; }
+
+void MyWindow::ToggleFullscreen() // read more about ... kinda understand .. window gets bigger ish
+{
+	isFullscreen = !isFullscreen;
+	Destroy();
+	Create();
+}
+
+void MyWindow::WUpdate()
 {
 	Event event; // this is so the buttons we press works .. something, something ..
 	while (window.pollEvent(event)) // don't know what any of this does. Nothing changes when you comment it out??
@@ -56,28 +69,19 @@ void MyWindow::WUpdate()
 		{
 			cout << event.type << endl;
 			isDone = true;
-			
+
 		}
-		else if (event.type == Event::KeyPressed&&event.key.code == Keyboard::F5)
+		else if (event.type == Event::KeyPressed && event.key.code == Keyboard::F5)
 		{
 			ToggleFullscreen();
 		}
 	}
 }
-void MyWindow::ToggleFullscreen() // read more about ... kinda understand .. window gets bigger ish
-{
-	isFullscreen = !isFullscreen;
-	Destroy();
-	Create();
-}
-void MyWindow::BeginDraw() { window.clear(Color::Black); }
-void MyWindow::EndDraw() { window.display(); }
 
-bool MyWindow::IsDone() { return isDone; }
-bool MyWindow::IsFullscreen() { return isFullscreen; }
-Vector2u MyWindow::GetWindowSize() { return windowSize; }
 
-void MyWindow::Draw(Drawable& drawable) 
-{
-	window.draw(drawable);
-}
+
+
+//void MyWindow::Draw(Drawable& drawable) 
+//{
+//	window.draw(drawable);
+//}
